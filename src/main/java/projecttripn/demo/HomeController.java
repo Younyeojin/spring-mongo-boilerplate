2180c6b30379c085747be26b2064b5ceb748fc51
Younyeojin <yjyoon2107@gmail.com>
2021-08-11 11:40:55 +0900

    1

---
+++ src/main/java/projecttripn/demo/HomeController.java
@@ -0,0 +1,17 @@
+package projecttripn.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date());
+    }
+}
+
+
